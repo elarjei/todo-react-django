6304ac49a2fbf75e0d129756da0b3da9e25a95cd
jagad <elarjeigidigiem@gmail.com>
2021-01-10 11:47:30 +0700

    fix serviceWorker enable ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -27,24 +27,24 @@ class App extends Component {
   }
   displayCompleted = status => {
     if (status) {
-      return this.setState({  viewCompleted: true });
+      return this.setState({ viewCompleted: true });
     }
     return this.setState({ viewCompleted: false });
-  }
+  };
   renderTabList = () => {
     return (
-      <div className='my-5 tab-list'>
+      <div className="my-5 tab-list">
         <span
           onClick={() => this.displayCompleted(true)}
-          className={(this.state.viewCompleted ? 'active' : '')}
+          className={this.state.viewCompleted ? "active" : ""}
         >
-          Selesai
+          complete
         </span>
         <span
           onClick={() => this.displayCompleted(false)}
-          className={(this.state.viewCompleted ? '' : 'active')}
+          className={this.state.viewCompleted ? "" : "active"}
         >
-          Belum Selesai
+          Incomplete
         </span>
       </div>
     );
@@ -52,39 +52,41 @@ class App extends Component {
   renderItems = () => {
     const { viewCompleted } = this.state;
     const newItems = this.state.todoList.filter(
-      item => item.completed == viewCompleted
+      item => item.completed === viewCompleted
     );
     return newItems.map(item => (
       <li
-        key = { item.id }
-        className = 'list-group-item d-flex justify-content-between align-items-center'
+        key={item.id}
+        className="list-group-item d-flex justify-content-between align-items-center"
       >
         <span
-          className = { 'todo-title mr-2 ${this.state.viewCompleted ? "completed-todo" : ""}' }
-          title = { item.description }
+          className={`todo-title mr-2 ${
+            this.state.viewCompleted ? "completed-todo" : ""
+          }`}
+          title={item.description}
         >
-          { item.title }
+          {item.title}
         </span>
         <span>
-          <button className = 'btn btn-secondary mr-2'>Ubah</button>
-          <button className = 'btn btn-danger'>Hapus</button>
+          <button className="btn btn-secondary mr-2"> Edit </button>
+          <button className="btn btn-danger">Delete </button>
         </span>
       </li>
     ));
   };
   render() {
     return (
-      <main className = 'content'>
-        <h1 className = 'text-white text-uppercase text-center my-4'>Todo List</h1>
-        <div className = 'row'>
-          <div className = 'col-md-6 col-sm-10 mx-auto p-0'>
-            <div className = 'card p-3'>
-              <div className = ''>
-                <button className = 'btn btn-primary'>Buat Agenda</button>
+      <main className="content">
+        <h1 className="text-white text-uppercase text-center my-4">Todo app</h1>
+        <div className="row ">
+          <div className="col-md-6 col-sm-10 mx-auto p-0">
+            <div className="card p-3">
+              <div className="">
+                <button className="btn btn-primary">Add task</button>
               </div>
-              { this.renderTabList() }
-              <ul className = 'list-group list-group-flush'>
-                { this.renderItems() }
+              {this.renderTabList()}
+              <ul className="list-group list-group-flush">
+                {this.renderItems()}
               </ul>
             </div>
           </div>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-// import servicWorkers from './servicWorkers';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -16,4 +17,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-// serviceWorker.unregister();
+serviceWorker.register()
